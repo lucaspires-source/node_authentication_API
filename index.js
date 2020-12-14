13927e5d85a057e0b5acd56caba934f41359dbaf
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-12-14 08:39:54 -0300

    started developing routes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+
+//Import Router
+const authoRoute = require('./routes/auth');
+
+
+app.listen(3000,() => console.log('Server is running'));
