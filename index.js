d35a2ff5aa7cd886669aa0d154f0baa79bf764d8
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-12-14 12:00:23 -0300

    established connection with DB

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongooose = require("mongoose");
+
+//Connect to DB
+mongooose.connect(
+  "mongodb+srv://lucas_r_pires:<TOTO4frica>@cluster0.46fmg.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  {useNewUrlParser:true},
+  ()=> console.log('connect to DB')
+);
 
 //Import Router
-const authoRoute = require('./routes/auth');
+const authRoute = require('./routes/auth');
 
+//Route Middlewares
+app.use('/api/user', authRoute);
 
-app.listen(3000,() => console.log('Server is running'));
+app.listen(3000, () => console.log('Server is running'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const router =require('express').Router();
 
 
-router.post('/register', (req,res =>{
+router.post('/register', (req,res) =>{
+    
     res.send('Register')
-}))
+})
+
+
+
 module.exports = router
