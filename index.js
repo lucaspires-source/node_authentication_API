6f6b743076fead19bde1a9706d34f5c5e269692c
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-12-14 13:23:39 -0300

    bug fixies

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const mongooose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
 
 //Connect to DB
 mongooose.connect(
-  "mongodb+srv://lucas_r_pires:<TOTO4frica>@cluster0.46fmg.mongodb.net/<dbname>?retryWrites=true&w=majority",
+   process.env.DB_CONNECT, 
   {useNewUrlParser:true},
   ()=> console.log('connect to DB')
 );
